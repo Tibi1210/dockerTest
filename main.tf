60279dd06b634b143accab16d4fad3c296ea6bef
Vajda Tibor <h053353@stud.u-szeged.hu>
2024-11-27 16:32:54 +0100

    try

--- main.tf
+++ main.tf
@@ -2,28 +2,16 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.0"
+      version = "~> 3.0.2"
     }
   }
 }
 
 resource "docker_image" "node_app" {
-  name = "nodejs-sample-app:latest"
+  name = "nodejs-sample-app"
   build {
     context    = "."
     dockerfile = "Dockerfile_deploy-env"
     tag        = ["nodejs-sample-app:latest"]
   }
-}
-
-resource "docker_container" "node_app" {
-  name  = "nodejs-sample-app"
-  image = docker_image.node_app.image_id
-
-  ports {
-    internal = 3000
-    external = 3000
-  }
-
-  restart = "unless-stopped"
 }
