c875eabde9f70ee95fffc364d9a845a0edfbee20
Vajda Tibor <h053353@stud.u-szeged.hu>
2024-11-27 15:50:08 +0100

    updated for dev-env

--- main.tf
+++ main.tf
@@ -7,8 +7,23 @@ terraform {
   }
 }
 
-provider "docker" {}
+resource "docker_image" "node_app" {
+  name = "nodejs-sample-app:latest"
+  build {
+    context    = "."
+    dockerfile = "Dockerfile_deploy-env"
+    tag        = ["nodejs-sample-app:latest"]
+  }
+}
 
-resource "docker_image" "node" {
-  name = "node:14"
+resource "docker_container" "node_app" {
+  name  = "nodejs-sample-app"
+  image = docker_image.node_app.image_id
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+
+  restart = "unless-stopped"
 }
