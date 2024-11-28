d59fdcd5aa231a776b1356cce57be7cfbaaf0d37
Vajda Tibor <h053353@stud.u-szeged.hu>
2024-11-28 14:00:49 +0100

    docker contained added

--- main.tf
+++ main.tf
@@ -9,6 +9,18 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "node" {
+resource "docker_image" "nodeImg" {
   name = "localhost:5000/nodejs-sample-app"
+}
+
+resource "docker_container" "nodeCont" {
+  name  = "nodejs-sample-app"
+  image = docker_image.nodeImg.image_id
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+
+  restart = "unless-stopped"
 }
